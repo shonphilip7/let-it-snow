45f1115edea58a00c1477b66a9d7cf6e0cd19205
Shon Philip <shonphilip7@gmail.com>
2017-11-18 13:40:31 -0500

    first commit

---
+++ js/let_it_snow.js
@@ -0,0 +1,6 @@
+jQuery( document ).ready(function(){
+	jQuery('body').flurry({height:Drupal.settings.let_it_snow.let_it_snow_height,frequency:Drupal.settings.let_it_snow.let_it_snow_frequency,
+	speed:Drupal.settings.let_it_snow.let_it_snow_speed,small:Drupal.settings.let_it_snow.let_it_snow_small,
+	large:Drupal.settings.let_it_snow.let_it_snow_large,wind:Drupal.settings.let_it_snow.let_it_snow_wind,
+	windVariance:Drupal.settings.let_it_snow.let_it_snow_variance});
+})
