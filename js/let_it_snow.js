0d4c312c0ea2fdd69f1b1a5805f857902017fa12
Shon Philip <shonphilip7@gmail.com>
2018-02-02 09:32:23 -0500

    add comments

--- js/let_it_snow.js
+++ js/let_it_snow.js
@@ -1,6 +1,11 @@
-jQuery( document ).ready(function(){
-	jQuery('body').flurry({height:Drupal.settings.let_it_snow.let_it_snow_height,frequency:Drupal.settings.let_it_snow.let_it_snow_frequency,
+  /**
+   * @file Module's main JavaScript file 
+   */
+  
+  //calling the jQuery plugin when page is ready
+  jQuery( document ).ready(function(){
+    jQuery('body').flurry({height:Drupal.settings.let_it_snow.let_it_snow_height,frequency:Drupal.settings.let_it_snow.let_it_snow_frequency,
 	speed:Drupal.settings.let_it_snow.let_it_snow_speed,small:Drupal.settings.let_it_snow.let_it_snow_small,
 	large:Drupal.settings.let_it_snow.let_it_snow_large,wind:Drupal.settings.let_it_snow.let_it_snow_wind,
 	windVariance:Drupal.settings.let_it_snow.let_it_snow_variance});
-})
+  })
